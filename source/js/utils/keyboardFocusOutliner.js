805b55bda8b9c493e9fa03c61c57ab5c4022de0b
Robert Krieg <hello@robertkrieg.ch>
2017-08-10 18:56:26 +0200

    Add utility to add ".outline" class to focused element when triggered with keyboard (+ accessability)

---
+++ source/js/utils/keyboardFocusOutliner.js
@@ -0,0 +1,32 @@
+/**
+ * Adds ".outline" class to elements, when they get focused using keyboard.
+ * This way, we can disable common, ugly ":focus { outline: ... }" styles
+ * add add custom focus style for tabkey navigation to increase accessability
+ */
+
+export default function() {
+
+    const TABKEY = 9;
+
+    //
+    document.addEventListener('keydown', (e) => {
+        if(e.keyCode == TABKEY) {
+            window.setTimeout( () => {
+                let outlinedElements = document.querySelectorAll('.outline');
+                for(let outlinedElement of outlinedElements) {
+                    outlinedElement.classList.remove('outline');
+                }
+                document.activeElement.classList.add('outline');
+            }, 50);
+        }
+    });
+
+
+    //
+    document.addEventListener('focusout', (e) => {
+        let outlinedElements = document.querySelectorAll('.outline');
+        for(let outlinedElement of outlinedElements) {
+            outlinedElement.classList.remove('outline');
+        }
+    });
+}
