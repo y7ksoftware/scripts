3a1e0892615dafa8ccc7b13abdd58d828e080864
Robert Krieg <hello@robertkrieg.ch>
2018-10-02 14:47:38 +0200

    simplify StateScroll and remove unnecessary actions (synchronized mutations are enough)

--- src/js/store/StateScroll.js
+++ src/js/store/StateScroll.js
@@ -12,12 +12,16 @@ const state = {
 // MUTATIONS
 // ------------------------------------------------------------------------
 const mutations = {
-
     /**
      * Enable or disable scroll
      */
     setIsScrollEnabled(state, isScrollEnabled) {
         state.isScrollEnabled = isScrollEnabled;
+        if (isScrollEnabled) {
+            enableScroll();
+        } else {
+            disableScroll();
+        }
     },
 };
 
@@ -27,25 +31,6 @@ const mutations = {
 // ------------------------------------------------------------------------
 const actions = {
 
-    /**
-     * Enable body scroll (restore position)
-     */
-    enableScroll({ commit }) {
-        if (!state.isScrollEnabled) {
-            commit('setIsScrollEnabled', true);
-            enableScroll();
-        }
-    },
-
-    /**
-     * Disable body scroll (keep position)
-     */
-    disableScroll({ commit }) {
-        if (state.isScrollEnabled) {
-            commit('setIsScrollEnabled', false);
-            disableScroll();
-        }
-    },
 };
 
 
