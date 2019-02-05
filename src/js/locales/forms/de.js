6150e0ab52415bed3fab3210f2eb50aef6f116e5
Robert Krieg <hello@robertkrieg.ch>
2019-02-05 08:33:47 +0100

    updatr packages and form validation messages

---
+++ src/js/locales/forms/de.js
@@ -0,0 +1,16 @@
+export default {
+    de: {
+        messages: {
+            required: field => `${field} ist ein Pflichtfeld.`,
+        },
+        custom: {
+            email: {
+                required: 'Bitte geben Sie eine Email–Adresse an.',
+                email: 'Bitte geben Sie eine gültige Email–Adresse an.',
+            },
+            name: {
+                required: 'Bitte geben Sie einen Namen an.',
+            },
+        },
+    },
+};
---
+++ src/js/locales/forms/en.js
@@ -0,0 +1,16 @@
+export default {
+    en: {
+        messages: {
+            required: field => `${field} is required.`,
+        },
+        custom: {
+            email: {
+                required: 'Please provide an email address.',
+                email: 'Please provide a valid email address.',
+            },
+            name: {
+                required: 'Please provide a name.',
+            },
+        },
+    },
+};
