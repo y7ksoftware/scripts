778567799d14963fe2f463d257e0e8914677d27a
Robert Krieg <hello@robertkrieg.ch>
2019-03-04 17:32:05 +0100

    adjust url helper

--- src/js/utils/url.js
+++ src/js/utils/url.js
@@ -2,7 +2,17 @@
 /**
  *
  */
-export function buildQueryUrl(url, keyValuePairs) {
-    const queryString = Object.keys(keyValuePairs).map(key => `${key}=${keyValuePairs[key]}`).join('&');
+export function buildQueryString(keyValuePairs, encodeValues = false) {
+    return Object.keys(keyValuePairs).map((key) => {
+        return `${key}=${encodeValues ? encodeURIComponent(keyValuePairs[key]) : keyValuePairs[key]}`;
+    }).join('&');
+}
+
+
+/**
+ *
+ */
+export function buildQueryUrl(url, keyValuePairs, encodeValues = false) {
+    const queryString = buildQueryString(keyValuePairs, encodeValues);
     return url + (queryString ? `?${queryString}` : '');
 }
